c021fc04fdebafea0855d46ca3dd0d72fa06d8df
Kyman-Irena <31901007+Kyman-Irena@users.noreply.github.com>
2018-05-27 07:30:08 +0300

    topic 5

--- scripts/topic-6.js
+++ scripts/topic-6.js
@@ -1,41 +1,45 @@
-function addButton() {
-    var div = document.querySelector('div');
-    var btn = document.createElement("BUTTON");
-    btn.textContent = 'A New Button';
-    btn.classList.add('submission')
-    btn.setAttribute('id', 'addedButton')
-    div.appendChild(btn);
+function getRandInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function removeButton(){
-    var p = document.getElementById('addedButton');
-    p.parentNode.removeChild(p);
+
+function randomColor() {
+  return `${getRandInt(0,255)}, ${getRandInt(0,255)}, ${getRandInt(0,255)}`;
+}
+
+
+function removeDescr(){
+    var h = document.querySelector('h4');
+    h.parentNode.removeChild(h);
 }
 
 function changeColor() {
-    var pars = document.querySelectorAll('p');
-    var btn = document.createElement("BUTTON");
-    btn.textContent = 'A New Button';
-    btn.classList.add('submission')
-    for (var p of pars)
-    p.style.color = 'blue';
+    var heads = document.querySelectorAll('h4');
+    for (var h of heads)
+    h.style.color = 'red' ;
 }
 function addDBefore(){
     var newEl = document.createElement('div');
-    newEl.innerHTML = '<p>Here is my best friend, his name is Aral</p>';
+    newEl.innerHTML = '<h4>Here is my best friend, his name is Aral</h4>';
     var div = document.getElementById('img');
     var img = document.querySelector('img');
     div.insertBefore(newEl, img);
 }
 function addDAfter(){
     var newEl = document.createElement('div');
-    newEl.innerHTML = '<p>Here is my best friend, his name is Aral</p>';
+    newEl.innerHTML = '<h4>Here is my best friend, his name is Aral</h4>';
     var div = document.getElementById('img');
     var img = document.querySelector('img');
     div.insertBefore(newEl, img.nextSibling);
 }
 function addDAppendChild() {
     var newEl = document.createElement('div');
-    newEl.innerHTML = '<p>Here is my best friend, his name is Aral</p>';
+    newEl.innerHTML = '<img src="../img/Aral.jpg" alt="a horse" >';
     var div = document.getElementById('img');
+    newEl.setAttribute('id', 'img');
     div.appendChild(newEl);
+}
+
+function addFrame() {
+var img = document.querySelector('img');
+    img.classList.add('frame');
 }
