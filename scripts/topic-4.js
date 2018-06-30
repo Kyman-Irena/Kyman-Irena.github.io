a5995bcbcc3c131669117dcb121d8c3558ab21ca
Kyman-Irena <31901007+Kyman-Irena@users.noreply.github.com>
2018-06-30 17:28:10 +0300

    +

--- scripts/topic-4.js
+++ scripts/topic-4.js
@@ -5,13 +5,13 @@ function getRank() {
     let request = new XMLHttpRequest();
 
     request.onreadystatechange = function() {
-        // if (request.readyState == 1) {
-          //   alert("1.Server Connection has been established!");
-       //  } else if (request.readyState == 2) {
-         //    alert("2.Your request was received by the server!");
-         //} else if (request.readyState == 3) {
-           //  alert("3.Request is being processed by the server");
-         //} else 
+         if (request.readyState == 1) {
+           alert("1.Server Connection has been established!");
+         } else if (request.readyState == 2) {
+           alert("2.Your request was received by the server!");
+       } else if (request.readyState == 3) {
+         alert("3.Request is being processed by the server");
+       } else 
         if (request.readyState == 4 && request.status == 200) {
             //alert("4.Responce has been sent and is ready to process");
             let pair = JSON.parse(request.responseText);
@@ -22,7 +22,7 @@ function getRank() {
         // }
     };
 
-    request.open("GET", "../topics/dressage.json", true);
+    request.open("GET", "http://ikyman.com/cit261/dressage.json", true);
     request.send();
 }
 
